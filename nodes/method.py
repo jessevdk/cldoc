496e91ad4332614f00b24fb42e90cece83c6eda5
Jesse van den Kieboom <jessevdk@gnome.org>
2013-02-06 01:01:34 +0100

    Set method abstract depending on whether a definition is found

--- nodes/method.py
+++ nodes/method.py
@@ -10,4 +10,15 @@ class Method(Function):
         self.static = cursor.is_static_method()
         self.virtual = cursor.is_virtual_method()
 
+        self.abstract = True
+        self.update_abstract(cursor)
+
+    def update_abstract(self, cursor):
+        if cursor.is_definition() or cursor.get_definition():
+            self.abstract = False
+
+    def add_ref(self, cursor):
+        super(Method, self).add_ref(cursor)
+        self.update_abstract(cursor)
+
 # vi:ts=4:et
