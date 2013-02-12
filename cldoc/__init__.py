b26b1dbfd81a8cdae31999170cc4d3e0aaab4e07
Jesse van den Kieboom <jessevdk@gnome.org>
2013-02-12 16:40:02 +0100

    Allow host:port argument for --serve

--- cldoc/__init__.py
+++ cldoc/__init__.py
@@ -39,8 +39,6 @@ def serve(opts):
         sys.stderr.write("Please specify the output directory to serve\n")
         sys.exit(1)
 
-    url = 'http://localhost:6060/'
-
     class Server(SocketServer.TCPServer):
         allow_reuse_address = True
 
@@ -56,10 +54,17 @@ def serve(opts):
             pass
 
     class SocketThread(threading.Thread):
-        def __init__(self):
+        def __init__(self, host):
             threading.Thread.__init__(self)
 
-            self.httpd = Server(("", 6060), Handler)
+            if not ':' in host:
+                self.host = host
+                self.port = 6060
+            else:
+                self.host, port = host.split(':')
+                self.port = int(port)
+
+            self.httpd = Server((self.host, self.port), Handler)
 
         def shutdown(self):
             self.httpd.shutdown()
@@ -68,10 +73,15 @@ def serve(opts):
         def run(self):
             self.httpd.serve_forever()
 
-    t = SocketThread()
+    t = SocketThread(opts.serve)
     t.start()
 
     dn = open(os.devnull)
+
+    if t.host == '':
+        url = 'http://localhost:{0}/'.format(t.port)
+    else:
+        url = 'http://{0}:{1}/'.format(t.host, t.port)
 
     if sys.platform.startswith('darwin'):
         subprocess.call(('open', url), stdout=dn, stderr=dn)
@@ -94,12 +104,14 @@ def inspect(opts, cxxflags):
     inspecttree.inspect(t)
 
 def run():
-    if not '--help' in sys.argv:
-        try:
-            sep = sys.argv.index('--')
-        except ValueError:
+    try:
+        sep = sys.argv.index('--')
+    except ValueError:
+        if not '--help' in sys.argv:
             sys.stderr.write('Please use: cldoc [CXXFLAGS] -- [OPTIONS] [FILES]\n')
             sys.exit(1)
+        else:
+            sep = 0
 
     parser = argparse.ArgumentParser(description='clang based documentation generator.',
                                      usage='%(prog)s [CXXFLAGS] -- [OPTIONS] [FILES]')
@@ -110,14 +122,14 @@ def run():
     parser.add_argument('--report', default=False,
                         action='store_const', const=True, help='report documentation coverage and errors')
 
-    parser.add_argument('--serve', default=False,
-                        action='store_const', const=True, help='serve generated documentation')
+    parser.add_argument('--serve', default=None, nargs='?', const=':6060', metavar='HOST:PORT',
+                        help='serve generated documentation (default on :6060)')
 
     parser.add_argument('--output', default=None, metavar='DIR',
                         help='specify the output directory')
 
     parser.add_argument('--type', default='html', metavar='TYPE',
-                        help='specify the type of output (html or xml)')
+                        help='specify the type of output (html or xml, default html)')
 
     parser.add_argument('--merge', default=None, metavar='FILES',
                         help='specify additional description files to merge into the documentation')
@@ -125,7 +137,7 @@ def run():
     parser.add_argument('--basedir', default=None, metavar='DIR',
                         help='the project base directory')
 
-    parser.add_argument('files', nargs='+', help='files to parse')
+    parser.add_argument('files', nargs='*', help='files to parse')
 
     args = sys.argv[sep + 1:]
 
