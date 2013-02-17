b5753c734ab83d4a707c07bf8dd80edc715dea11
Jesse van den Kieboom <jessevdk@gmail.com>
2013-02-17 16:45:40 +0100

    Move coding comment up

--- cldoc/inspecttree.py
+++ cldoc/inspecttree.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file is part of cldoc.  cldoc is free software: you can
 # redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, version 2.
@@ -10,7 +11,6 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc., 51
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-# -*- coding: utf-8 -*-
 
 from clang import cindex
 import os, sys
