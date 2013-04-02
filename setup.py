36bb855b7c5ea9da12a97a1bd7e580c219735d91
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2013-04-02 14:27:26 +0200

    Remove debug statement

--- setup.py
+++ setup.py
@@ -113,8 +113,6 @@ dataprefix = 'cldoc'
 for dirpath, dirnames, filenames in os.walk(os.path.join(dataprefix, 'data')):
     datafiles += [os.path.join(dirpath[len(dataprefix)+1:], f) for f in filenames]
 
-print datafiles
-
 setup(name='cldoc',
       version='1.4',
       description='clang based documentation generator for C/C++',
