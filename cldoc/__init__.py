dab786049011d23cf23421eeee42ea391f9eb1c5
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2013-06-02 14:15:36 +0200

    List available commands

--- cldoc/__init__.py
+++ cldoc/__init__.py
@@ -30,9 +30,20 @@ def run_gir(args):
     from . import cmdgir
     cmdgir.run(args)
 
+def print_available_commands():
+    sys.stderr.write('Available commands:\n')
+
+    commands = ['inspect', 'serve', 'generate', 'gir']
+
+    for c in commands:
+        sys.stderr.write('  ' + c + '\n')
+
+    sys.stderr.write('\n')
+
 def run():
     if len(sys.argv) <= 1:
-        sys.stderr.write('Please use: cldoc [command] [OPTIONS] [FILES...]\n')
+        sys.stderr.write('Please use: cldoc [command] [OPTIONS] [FILES...]\n\n')
+        print_available_commands()
         sys.exit(1)
 
     cmd = sys.argv[1]
@@ -46,8 +57,9 @@ def run():
         run_generate(rest)
     elif cmd == 'gir':
         run_gir(rest)
-    elif cmd == '--help':
-        sys.stderr.write('Please use: cldoc [command] --help\n')
+    elif cmd == '--help' or cmd == '-h':
+        sys.stderr.write('Please use: cldoc [command] --help\n\n')
+        print_available_commands()
         sys.exit(1)
     else:
         sys.stderr.write('Unknown command `{0}\'\n'.format(cmd))
