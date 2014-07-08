c409fe2ea9f02ce4aed9f8e48cccbc4a2e2eb74a
Jesse van den Kieboom <jessevdk@gnome.org>
2014-07-08 17:37:48 +0200

    Added template tests

---
+++ tests/input/template.hh
@@ -0,0 +1,19 @@
+/* The class A.
+ * @T the template parameter T.
+ *
+ * A longer description of A.
+ */
+template <typename T>
+class A
+{
+public:
+	/* The a function.
+	 * @TT the TT template.
+	 *
+	 * The long description of a.
+	 *
+	 * @return the value.
+	 */
+	template <typename TT>
+	int a(TT tt);
+};
