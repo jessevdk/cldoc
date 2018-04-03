5210e2aba3dfb4b36941432a2d333204e426c573
Jesse van den Kieboom <jkieboom@esri.com>
2018-04-03 08:55:28 +0200

    Release version 1.11.2

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ for dirpath, dirnames, filenames in os.walk(os.path.join(dataprefix, 'data')):
     datafiles += [os.path.join(dirpath[len(dataprefix)+1:], f) for f in filenames]
 
 setup(name='cldoc',
-      version='1.11.1',
+      version='1.11.2',
       description='clang based documentation generator for C/C++',
       author='Jesse van den Kieboom',
       author_email='jessevdk@gmail.com',
